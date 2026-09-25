bd9f2e99caef2dd72b78a62b67701ca58de97ae3
Jaswanth Reddy <ajaswanthreddy88@gmail.com>
2026-09-25 23:22:28 +0530

    Implement PasswordChecker class for password strength

---
+++ assign3.java
@@ -0,0 +1,36 @@
+class PasswordChecker {
+    private final String password;
+
+    // Constructor
+    PasswordChecker(String password) {
+        this.password = password;
+    }
+
+    // Check password strength
+    String getStrength() {
+
+        int length = password.length();
+
+        if (length < 6) {
+            return "Weak";
+        } else if (length <= 9) {
+            return "Medium";
+        } else {
+            return "Strong";
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        PasswordChecker pc = new PasswordChecker("abcd");
+        System.out.println(pc.getStrength());
+
+        PasswordChecker pc2 = new PasswordChecker("abcdefgh");
+        System.out.println(pc2.getStrength());
+
+        PasswordChecker pc3 = new PasswordChecker("abcdefghij");
+        System.out.println(pc3.getStrength());
+    }
+}
